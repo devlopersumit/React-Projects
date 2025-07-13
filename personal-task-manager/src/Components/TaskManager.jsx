cb06bdc17ac7c3dceb9fbdb65d584cebdc7c6ae1
devlopersumit <sumitsj2002@gmail.com>
2025-07-13 16:26:20 +0530

    Created checkbox for each taskitem

--- personal-task-manager/src/Components/TaskManager.jsx
+++ personal-task-manager/src/Components/TaskManager.jsx
@@ -18,7 +18,17 @@ const handleAddTask = (e)=> {
 
  setTasks(prev => [...prev, task]);
  setNewTask("");
+
+ 
 }
+
+ const toggleComplete = (id)=> {
+    const updatedTask = tasks.map(task => 
+        task.id === id ? {...task, completed:!task.completed} : task
+    );
+    setTasks(updatedTask);
+ };
+
 
     return (
         <>
@@ -26,11 +36,13 @@ const handleAddTask = (e)=> {
             <h1 className="text-white text-3xl font-bold mb-6">Personal Task Manager</h1>
             <div className="w-auto h-28 bg-white border-none rounded-xl px-6 py-4">
             <form onSubmit={handleAddTask}>
+
                 <input type="text"
                 placeholder="Enter the task..."
                 value={newTask}
                 onChange={(e)=> setNewTask(e.target.value)}
                 className="basis-1 h-auto p-1 rounded-xl outline-none border-solid border-[1px] border-gray-900 pl-4 text-xl"  />
+
                 <button className="w-auto h-auto py-2 px-5 ml-2 bg-purple-600 text-white rounded-xl font-semibold hover:bg-purple-700 duration-75 active:bg-purple-900" 
                 type="submit">Add Task</button>
             </form>
@@ -38,7 +50,12 @@ const handleAddTask = (e)=> {
             <ul>
                 {
                     tasks.map(task => (
-                        <li key={task.id}>{task.title}</li>
+                        <li key={task.id}>
+                            <input type="checkbox"
+                            checked = {task.completed}
+                            onChange={()=> toggleComplete(task.id)} />
+                            <span style={{textDecoration: task.completed ? "line-through" : "none"}}>{task.title}</span>
+                        </li>
                     ))
                 }
             </ul>
